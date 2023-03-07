0ac3b56954e9c93e607651fae9b867d0c5896ca0
shephali09 <hiremathshephali9@gmail.com>
2023-03-07 09:48:34 +0530

    Add:As a Simulator start with Flipping a Coin to Display Heads or Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,9 @@
-echo "Welcome to Flip coin combination program"
+random_number=$((RANDOM % 2))
+
+if [[ $random_number -eq 0 ]]; then
+    result="heads"
+else
+    result="tails"
+fi
+
+echo "The coin landed on: $result"
